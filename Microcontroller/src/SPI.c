f123de60667bb39096f0bfee515a302e02e0d8cc
Sigurd <sigurd@hestbech.dk>
2025-06-16 11:34:11 +0200

    spi baud-rate sat til 500k

--- Microcontroller/src/SPI.c
+++ Microcontroller/src/SPI.c
@@ -26,7 +26,7 @@ void master_init()
     PORTB |= (1 << PB0);                          // Set SS high (inactive)
 
     SPCR |= (1 << SPE) | (1 << MSTR) | (1 << DORD); // Enable SPI in master mode, (DORD = 1) makes so LSB transmits first
-    SPCR |= (1 << SPR0) | (1 << SPR1);              // Set SCK clock rate too fck/128 (S.203), 125 kHz with F_CPU = 16 MHz
+    SPCR |= (1 << SPI2X) | (1 << SPR1);             // Set SCK clock rate too fck/32 (S.203), 500kHz with F_CPU = 16 MHz(500 kilo baud)
     SPCR &= ~((1 << CPOL) | (1 << CPHA));           // Sample on rising edge, transmit on falling (S.200–202)
 }
 
